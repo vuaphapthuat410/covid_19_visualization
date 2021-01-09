d3fe870b24500151f7e8f0aac0a0c899c2387425
vuaphapthuat410 <vuaphapthuat410@gmail.com>
2021-01-09 18:01:17 +0700

    Change Graph.js code for real time data using API

--- src/pages/Graph.js
+++ src/pages/Graph.js
@@ -2,84 +2,102 @@ import React from "react";
 import "../App.css";
 import ReactApexChart from "react-apexcharts";
 
-function Graph() {
-  const series = [
-    {
-      name: "Cases",
-      data: [
-        555,
-        12038,
-        69030,
-        88369,
-        167466,
-        932638,
-        2055423,
-        3343777,
-        3845718,
-      ],
-    },
-    {
-      name: "Recovered",
-      data: [28, 284, 9394, 42710, 76026, 191853, 501538, 1029651, 1255481],
-    },
-    {
-      name: "Deaths",
-      data: [17, 259, 1666, 2996, 6472, 49675, 140658, 238619, 269567],
-    },
-  ];
-  const options = {
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    xaxis: {
-      type: "datetime",
-      categories: [
-        "1/22/20",
-        "2/1/20",
-        "2/15/20",
-        "3/1/20",
-        "3/15/20",
-        "4/1/20",
-        "4/15/20",
-        "5/1/20",
-        "5/7/20",
-      ],
-    },
-    tooltip: {
-      x: {
-        format: "dd/MM/yy",
-      },
-    },
+class Graph extends React.Component {
+  state = {
+    loading: true,
+    cases: null,
+    deaths: null,
+    recovered: null,
   };
 
-  return (
-    <div
-      style={{
-        backgroundColor: "white",
-        textAlign: "center",
-      }}
-    >
-      <br />
-      <h2>COVID-19 Global Graphs</h2>
-      <br />
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="area"
-        height={350}
-      />
-      <br />
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="bar"
-        height={350}
-      />
-    </div>
-  );
+  async componentDidMount() {
+    const url = "https://disease.sh/v3/covid-19/historical/all?lastdays=30";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ cases: data.cases, deaths: data.deaths, recovered: data.recovered, loading: false });
+    for (const [key, value] of Object.entries(this.state.cases)) {
+      console.log(`${key}: ${value}`);
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    }
+
+    function getValue(item) {
+      return Object.values(item);
+    }
+
+    var series = [
+      {
+        name: "Cases",
+        data: [],
+      },
+      {
+        name: "Recovered",
+        data: [],
+      },
+      {
+        name: "Deaths",
+        data: [],
+      },
+    ];
+
+    series[0].data = Object.entries(this.state.cases).map(getValue);
+
+    series[2].data = Object.entries(this.state.deaths).map(getValue);
+
+    series[1].data = Object.entries(this.state.recovered).map(getValue);  
+
+    var options = {
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy",
+        },
+      },
+    };
+
+    options.xaxis.categories = Object.entries(this.state.cases).map((item) => {return Object.keys(item)});
+
+    return (
+      <div
+        style={{
+          backgroundColor: "white",
+          textAlign: "center",
+        }}
+      >
+        <br />
+        <h2>COVID-19 Global Graphs</h2>
+        <br />
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="area"
+          height={350}
+        />
+        <br />
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="bar"
+          height={350}
+        />
+      </div>
+    );
+  }
 }
+
+
 
 export default Graph;
